94d11de301b557a5126db737370c082398aac2bf
Hugues Charleux <hcx@uhuru.dz>
2013-08-29 11:41:15 +0200

    Impoved doc and formating

--- Pboy/Config/Ini.php
+++ Pboy/Config/Ini.php
@@ -6,22 +6,36 @@ use Pboy\Component\Component;
 
 class Ini extends ConfigAbstract
 {
-
+    
     /**
-     * Default config file
+     * To be able to switch between config
+     * format we need to be able to use
+     * files regardeless of their suffix
+     * from outside this class.
+     *
+     * @var string
      */
     const SUFFIX        = '.ini';
-
+    
+    /**
+     * Path to config files
+     *
+     * @var string
+     */
     const PATH          = 'Conf/';
 
+    /**
+     * Storage array for our data
+     *
+     * @var array
+     */
     private $data = array();
 
 
     /**
-     * Loads configuration into the object
+     * Loads configuration into the storage array
      *
      * @param string $fileName  Path to config file
-     * @return Pboy\Config\Ini Object (to allow chain call)
      */
     public function load($fileName)
     {
@@ -34,12 +48,30 @@ class Ini extends ConfigAbstract
         $this->data[$fileName] = parse_ini_file($file, true);
     }
 
+
+    /**
+     * Loading is dynamic, the first depth of storage array
+     * being the files name.
+     * This allow us to : 
+     *  have an extra level with ini format
+     *  lazy load the needed files (thus, only used files)
+     *
+     * @param mixed string|int|null $offset  Asked array offset
+     * @return void
+     */
     private function loadIfNeeded($offset)
     {
         if (!$this->offsetExists($offset)) {
             $this->load($offset);
         }
     }
+
+    /* ____________________________________________________
+
+        Below is the basic ArrayAccess implementation
+        from php.net documentation.
+        See: http://php.net/ArrayAccess
+        ________________________________________________ */
 
     public function offsetSet($offset, $value) {
         if (is_null($offset)) {
@@ -48,13 +80,16 @@ class Ini extends ConfigAbstract
             $this->data[$offset] = $value;
         }
     }
+
     public function offsetExists($offset) {
         return isset($this->data[$offset]);
     }
 
+
     public function offsetUnset($offset) {
         unset($this->data[$offset]);
     }
+
 
     public function offsetGet($offset) {
         $this->loadIfNeeded($offset);
