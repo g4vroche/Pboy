997cff0e776385f5241673169a6d1e40c618f77c
Hugues Charleux <hcx@uhuru.dz>
2013-08-29 12:12:27 +0200

    Moved synopsis to 'controller' class

--- Pboy/Task/Jobs.php
+++ Pboy/Task/Jobs.php
@@ -20,4 +20,26 @@ class Jobs extends Component
         return true;
     }
 
+    public function synopsis()
+    {
+        $result = 'Pboy v.'.$this->Config['conf']['Pboy']['version']."\n";
+        $result .= "Tasks:\n";
+
+        foreach ($this->Config['tasks'] as $name => $params) {
+            
+            $result .= "\t$name: ". $params['description']."\n";
+
+            if (array_key_exists('arguments', $params)) {
+                $result .= "\tOptions:\n";
+                foreach ($params['arguments'] as $argument => $description) {
+                    $result .= "\t  $argument \t$description\n";
+                }
+            }
+            $result .="\t---\n";
+        }
+
+        return $result;
+
+        
+    }
 }
--- Pboy/Task/Task.php
+++ Pboy/Task/Task.php
@@ -86,28 +86,6 @@ class Task extends TaskAbstract
         return (strlen($option) > 1) ? 'long' : 'short';
     }
 
-    public function synopsis()
-    {
-        $result = 'Pboy v.'.$this->Config['conf']['Pboy']['version']."\n";
-        $result .= "Tasks:\n";
-
-        foreach ($this->Config['tasks'] as $name => $params) {
-            
-            $result .= "\t$name: ". $params['description']."\n";
-
-            if (array_key_exists('arguments', $params)) {
-                $result .= "\tOptions:\n";
-                foreach ($params['arguments'] as $argument => $description) {
-                    $result .= "\t  $argument \t$description\n";
-                }
-            }
-            $result .="\t---\n";
-        }
-
-        return $result;
-
-        
-    }
 }
 
 
