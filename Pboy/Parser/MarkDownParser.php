62c03b0478f2bd5f86369bd0a3c8c75bb1765c5b
Hugues Charleux <hcx@uhuru.dz>
2013-08-31 01:47:00 +0200

    use MarkDownExtra parser

--- Pboy/Parser/MarkDownParser.php
+++ Pboy/Parser/MarkDownParser.php
@@ -2,14 +2,14 @@
 
 namespace Pboy\Parser;
 
-use \Michelf\Markdown;
+use \Michelf\MarkdownExtra;
 
 class MarkDownParser extends ParserAbstract implements ParserInterface
 {
 
     public function parse($data)
     {
-        $data['content'] = Markdown::defaultTransform($data['content']);
+        $data['content'] = MarkdownExtra::defaultTransform($data['content']);
         
         return $data;
     }
