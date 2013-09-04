a8da3ab3270ebed0aeb5c201c66a01b52cb7b2b3
Hugues Charleux <hcx@uhuru.dz>
2013-09-04 23:33:34 +0200

    Improved UI

--- Pboy/Controller/Help.php
+++ Pboy/Controller/Help.php
@@ -10,6 +10,10 @@ class Help extends Component implements ControllerInterface
     public function run($options = array())
     {
         $result = $this->Io->format('Pboy v.'.$this->Config['conf']['Pboy']['version']);
+        
+        $result .= $this->Io->format('');
+        $result .= $this->Io->format($this->Config['conf']['Pboy']['synopsis']); 
+        $result .= $this->Io->format('');
 
 
         $result .= $this->Io->format("Tasks:");
@@ -17,8 +21,8 @@ class Help extends Component implements ControllerInterface
 
         foreach ($this->Config['tasks'] as $name => $params) {
             
-            $result .= $this->Io->format('# '.$name);
-            $result .= $this->Io->format($params['description']);
+            $result .= $this->Io->format('');
+            $result .= $this->Io->format("# $name: ".$params['description']);
 
             if (isset($params['arguments'])) {
                 $result .= $this->Io->format($this->Io->help($name));
