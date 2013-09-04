c3424ae2e77b039b57881d97abae63a5cdd2351a
Hugues Charleux <hcx@uhuru.dz>
2013-09-04 23:41:33 +0200

    refactoring Generate controller, with specific config

--- Pboy/Controller/Generate.php
+++ Pboy/Controller/Generate.php
@@ -10,47 +10,85 @@ class Generate extends Component implements ControllerInterface
     
     public function run($options = array())
     {
-
-        $pattern = $this->Config['providers']['FileSystem']['file_type_pattern'];
-
-        $items = $this->Input->getItems('posts', $pattern);
-
-
-
-
-        foreach ($items as $index => $item) {
-            $items[$index] = $this->Parser->parse($item);
+        foreach ($this->Config['views'] as $view => $params) {
+            if ($view != 'default') {
+                $this->process($this->getConfig($view));
+            }
         }
-        
-        $views = $this->Config['conf']['Rendering']['views'];        
-        $path  = $this->Config['tasks']['Generate']['output_path'];
-
-        $this->Renderer->setOutputPath($path);
-        $this->Renderer->setVariables($this->Config['conf']['Rendering']['template_vars']);
-
-        $assets = array(
-            'css' => $this->getAssets('css'),
-            'js'  => $this->getAssets('js')
-            );
-
-        $this->Renderer->setVariables($assets);
-
-        $this->Renderer->render($items, $views);
 
         return true;
     }
 
-    private function getAssets($type)
+
+    /**
+     * Retruns config with default setted
+     *
+     * @param string $view  View name
+     * @return array
+     */
+    private function getConfig($view)
     {
-        $assets = $this->Config['conf']['Rendering']['assets_'.$type];
-        $outputPath  = $this->Config['tasks']['Generate']['output_path'];
+        return array_merge(
+            $this->Config['views']['default'],
+            $this->Config['views'][$view]
+        );
+    }
 
-        $compress = $this->Config['conf']['Rendering']['compress_assets'];
+    public function process($cfg)
+    {
+        $formats = $this->Parser->supportedFormats();
 
-        return  $this->Assets->generate($assets, ucfirst($type), $outputPath, $compress);
+        $items = array();
+        foreach ($cfg['repositories'] as $repository) {
+            $items = array_merge($items, $this->fetchData($repository, $formats));
+        }
+
+        $assets = array(
+            'css' => $this->getAssets('css', $cfg),
+            'js'  => $this->getAssets('js', $cfg)
+            );
+
+        $this->Renderer->setOutputPath($cfg['output_path'])
+                       ->setVariables($cfg['template_vars'])
+                       ->setVariables($assets)
+                       ->renderView($cfg['template'], $cfg['type'], $items);
     }
 
 
+    /**
+     * 
+     * @param string $repository    An id to retrieve data
+     * @param array $formats        Handled document formats 
+     *      (avoid loading documents that won't be parsable)
+     * @return array
+     */
+    private function fetchData($repository, $formats)
+    {
+        $items = $this->Input->getItems($repository, $formats);
 
+        foreach ($items as $index => $item) {
+            $items[$index] = $this->Parser->parse($item);
+        }
+
+        return $items;
+    }
+
+
+    /**
+     * Generate assets if needed and return their paths
+     *
+     * @param string $type
+     * @param array $cfg
+     * @return array
+     */
+    private function getAssets($type, $cfg)
+    {
+        return  $this->Assets->generate(
+                    $cfg["assets_$type"], 
+                    ucfirst($type), 
+                    $cfg['output_path'], 
+                    $cfg['compress_assets']
+                );
+    }
 
 }
